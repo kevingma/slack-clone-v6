b9cc7caf7b50104d7d10e883259d9f575859dc0f
Kevin Ma <akevingma@gmail.com>
2025-01-07 00:20:13 -0800

    test

---
+++ src/auth/google.js
@@ -0,0 +1,21 @@
+import { defineUserSignupFields } from 'wasp/server/auth'
+
+export function googleConfig () {
+  return {
+    // Request both 'profile' and 'email' scopes.
+    scopes: ['profile', 'email']
+  }
+}
+
+export const googleSignupFields = defineUserSignupFields({
+  // In your schema, User.email is required. 
+  // We'll set it from the Google profile data.
+  email: (data) => {
+    if (!data.profile?.email) {
+      throw new Error('No email returned from Google. Check your scopes or the user\'s Google Account permissions.')
+    }
+    return data.profile.email
+  },
+  // Optionally, if you want to also set username from the Google profile:
+  // username: (data) => data.profile?.name || ''
+})
