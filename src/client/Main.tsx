239727fd829f49626703e9b7c3c9dded4e0b772f
Kevin Ma <akevingma@gmail.com>
2025-01-07 12:28:48 -0800

    Cosmetic improvements

--- src/client/Main.tsx
+++ src/client/Main.tsx
@@ -1,5 +1,4 @@
 import type { FC } from 'react'
-
 import './Main.css'
 import logo from './../../public/logo.webp'
 import { Link } from 'wasp/client/router'
@@ -8,8 +7,6 @@ import { Outlet, useLocation } from 'react-router-dom'
 
 export const Main: FC = () => {
   const { data: user, isLoading } = useAuth()
-  const username = user?.id || 'User'
-
   const location = useLocation()
   const currentPath = location.pathname
 
@@ -18,37 +15,51 @@ export const Main: FC = () => {
   return (
     <div className='min-h-screen flex flex-col'>
       <nav className='bg-gray-800 p-4'>
-        <div className='container mx-auto flex justify-between items-center'>
-          <Link to='/' className='flex-1 font-semibold text-yellow-300 hover:text-gray-300 transition-color'>
+        <div className='container mx-auto flex items-center'>
+          {/* Logo on the far left */}
+          <img
+            src={logo}
+            alt='Wasp Logo'
+            className='w-8 h-8 mr-2'
+          />
+
+          {/* Title text directly to the right of the logo */}
+          <Link
+            to='/'
+            className='text-yellow-300 hover:text-gray-300 transition-colors mr-auto'
+          >
             Slack Clone v5
           </Link>
-          <img src={logo} alt='Wasp Logo' className='w-8 h-8' />
-          <div className='flex-1 flex justify-end gap-6'>
+
+          {/* Hide "Chat" nav button if we are on the /chat page */}
+          {!isChatPage && (
             <Link
               to='/chat'
-              className={`hover:text-gray-300 transition-colors ${!isChatPage ? 'text-yellow-300' : 'text-white'}`}
+              className='text-yellow-300 hover:text-gray-300 transition-colors mr-4'
             >
               Chat
             </Link>
-            {user ? (
-              <div className='flex items-center gap-4'>
-                <button onClick={() => logout()} className='text-white hover:text-gray-300 transition-colors'>
-                  Logout, {username}
-                </button>
-              </div>
-            ) : (
-              <Link to='/login' className='text-white hover:text-gray-300 transition-colors'>
-                Login
-              </Link>
-            )}
-          </div>
+          )}
+
+          {/* Login / Logout */}
+          {user ? (
+            <button
+              onClick={() => logout()}
+              className='text-white hover:text-gray-300 transition-colors'
+            >
+              Logout, {user.email}
+            </button>
+          ) : (
+            <Link to='/login' className='text-white hover:text-gray-300 transition-colors'>
+              Login
+            </Link>
+          )}
         </div>
       </nav>
+
       <main className='flex-1'>
         <Outlet />
       </main>
     </div>
   )
 }
-
-export const linkStyles = 'text-yellow-300 hover:text-gray-300 transition-colors'
