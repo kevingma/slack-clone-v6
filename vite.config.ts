0d0090f0155bf3d702d44e2c27bafeb0f831fabb
Kevin Ma <akevingma@gmail.com>
2025-01-09 00:42:24 -0800

    prep for reboot

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolveProjectPath } from 'wasp/dev'
 
 export default defineConfig({
   server: {
     open: true,
+    fs: {
+      allow: [
+        // Allow the project root
+        resolveProjectPath('.'),
+        // Also allow the .wasp folder
+        resolveProjectPath('.wasp')
+      ]
+    }
   },
 })
